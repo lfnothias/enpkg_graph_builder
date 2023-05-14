f34e08406a38fc20a97dab38fb9d2b24665dac3a
lfnothias <louisfelix.nothias@gmail.com>
2023-05-14 23:21:29 +0200

    Adding a script to generate an RDF from molecular network and spectral library results a

--- src/individual_processing/06_rdf_individual_mn_indi.py
+++ src/individual_processing/06_rdf_individual_mn_indi.py
@@ -34,9 +34,11 @@ ionization_mode = args.ionization_mode
 
 path = os.path.normpath(sample_dir_path)
 
-samples_dir = [directory for directory in os.listdir(path)]
+samples_dir = [directory for directory in os.listdir(path) if directory != '.DS_Store']
+
 df_list = []
 for directory in tqdm(samples_dir):
+    print(directory)
     
     g = Graph()
     nm = g.namespace_manager
@@ -46,12 +48,18 @@ for directory in tqdm(samples_dir):
     nm.bind(prefix, ns_kg)
 
     graph_path = os.path.join(path, directory, ionization_mode, 'molecular_network', directory + '_mn_' + ionization_mode + '.graphml')
+    print(graph_path)
     graph_metadata_path = os.path.join(path, directory, ionization_mode, 'molecular_network', directory + '_mn_metadata_' + ionization_mode + '.tsv')
-    metadata_path = os.path.join(path, directory, directory + '_metadata.tsv')
+    print(graph_metadata_path)
+    spectral_lib_path = os.path.join(path, directory, ionization_mode, 'spectral_lib_matching', directory + '_lib_results_pos_final_' + ionization_mode + '.tsv')
+    print(spectral_lib_path)
+    metadata_path = os.path.join(path, directory, 'metadata.tsv')
+    print(metadata_path)
     try:
         graph = nx.read_graphml(graph_path)
         metadata = pd.read_csv(metadata_path, sep='\t')
         graph_metadata = pd.read_csv(graph_metadata_path, sep='\t')
+        spectral_lib_metadata = pd.read_csv(spectral_lib_path, sep='\t')
     except FileNotFoundError:
         continue
     except NotADirectoryError:
@@ -85,6 +93,21 @@ for directory in tqdm(samples_dir):
             g.add((link_node, ns_kg.has_member_1, t_feature_id))
             g.add((link_node, ns_kg.has_member_2, s_feature_id))
 
+        for _, row in spectral_lib_metadata.iterrows():
+            usi = 'mzspec:' + str(metadata['massive_id'][0]) + ':' + str(metadata.sample_id[0]) + '_features_ms2_'+ str(ionization_mode)+ '.mgf:scan:' + str(row['feature_id'])
+            feature_id = rdflib.term.URIRef(kg_uri + 'lcms_feature_' + usi)        
+            speclib_annotation_id = rdflib.term.URIRef(kg_uri + "speclib_" + usi)
+            g.add((feature_id, ns_kg.has_speclib_annotation, speclib_annotation_id))
+            g.add((speclib_annotation_id, RDFS.label, rdflib.term.Literal(f"spectral library annotation of {usi}")))
+            g.add((speclib_annotation_id, ns_kg.has_InChIkey, rdflib.term.URIRef(kg_uri + row['inchikey'])))
+            g.add((speclib_annotation_id, ns_kg.has_SMILES, rdflib.term.Literal(row['smiles'])))
+            g.add((speclib_annotation_id, ns_kg.has_structure_name, rdflib.term.Literal(row['compound_name'])))
+            g.add((speclib_annotation_id, ns_kg.has_inchi, rdflib.term.Literal(row['inchi'])))
+            g.add((speclib_annotation_id, ns_kg.has_spectral_library_id, rdflib.term.Literal(row['Spectral_library_ID'])))
+            g.add((speclib_annotation_id, ns_kg.has_spectral_library, rdflib.term.Literal(row['Spectral_library'])))
+            g.add((speclib_annotation_id, ns_kg.has_msms_score, rdflib.term.Literal(row['msms_score'], datatype=XSD.float)))
+            g.add((speclib_annotation_id, ns_kg.has_msms_score, rdflib.term.Literal(row['matched_peaks'], datatype=XSD.float)))
+    
     pathout = os.path.join(sample_dir_path, directory, "rdf/")
     os.makedirs(pathout, exist_ok=True)
     pathout = os.path.normpath(os.path.join(pathout, f'individual_mn_{ionization_mode}.ttl'))
