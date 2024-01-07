3c17889296a30999927a53eba8f6706671cf6602
lfnothias <louisfelix.nothias@gmail.com>
2024-01-07 22:29:50 +0100

    rdf_merger update to be more flexible
    Merge all RDF in the rdf folder

--- src/individual_processing/09_rdf_merger.py
+++ src/individual_processing/09_rdf_merger.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import rdflib
 import argparse
 import textwrap
+import glob
 
 # These lines allows to make sure that we are placed at the repo directory level 
 p = Path(__file__).parents[2]
@@ -54,16 +55,18 @@ for directory in tqdm(samples_dir):
 
     # Iterate over the files and add their contents to the merged graph
     exist_files = []
-    for file_path in ["rdf/canopus_pos.ttl", "rdf/canopus_neg.ttl", 
-                      "rdf/features_pos.ttl", "rdf/features_neg.ttl", 
-                      "rdf/features_spe2vec_pos.ttl", "rdf/features_spe2vec_neg.ttl",
-                      "rdf/indivual_mn_pos.ttl", "rdf/indivual_mn_neg.ttl",
-                      "rdf/isdb_pos.ttl", "rdf/isdb_neg.ttl",
-                      "rdf/sirius_pos.ttl", "rdf/sirius_neg.ttl"
-                      "rdf/metadata_enpkg.ttl", "rdf/metadata_module_enpkg.ttl", "rdf/structures_metadata.ttl"]:
-        if os.path.isfile(os.path.join(sample_dir_path, directory, file_path)):
-            exist_files.append(os.path.join(sample_dir_path, directory, file_path))
-    if len(exist_files) > 0:
+
+    # Define a pattern for the file types you're interested in
+    pattern = os.path.join(sample_dir_path, directory, 'rdf', '*.ttl')
+
+    # Use glob to get a list of file paths matching the pattern
+    files = glob.glob(pattern)
+
+    # Use a list comprehension to filter out non-existent files
+    exist_files = [file for file in files if os.path.isfile(file)]
+
+    # If there are any existing files, read their contents and parse them
+    if exist_files:
         for file_path in exist_files:
             with open(file_path, "r") as f:
                 file_content = f.read()
